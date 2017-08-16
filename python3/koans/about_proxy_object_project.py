668f5815a9f995b9fbbb2c3abf4b76bf071559b0
kevin00000000 <jiannan_yang@163.com>
2017-08-16 14:41:26 +0800

    all complete

--- python3/koans/about_proxy_object_project.py
+++ python3/koans/about_proxy_object_project.py
@@ -21,11 +21,31 @@ from runner.koan import *
 class Proxy:
     def __init__(self, target_object):
         # WRITE CODE HERE
-
+        self._messages = []
         #initialize '_obj' attribute last. Trust me on this!
         self._obj = target_object
 
     # WRITE CODE HERE
+    def __getattr__(self, attr_name):
+        self._messages.append(attr_name)
+        return getattr(self._obj, attr_name)
+    
+    def __setattr__(self, attr_name, value):
+        if attr_name!='_obj' and attr_name!='_messages':
+            self._messages.append(attr_name)
+            setattr(self._obj, attr_name, value)
+        else:
+            object.__setattr__(self, attr_name, value)
+
+    def messages(self):
+        return self._messages
+
+    def was_called(self, attr_name):
+        return attr_name in self._messages
+
+    def number_of_times_called(self, attr_name):
+        return self._messages.count(attr_name)
+
 
 # The proxy object should pass the following Koan:
 #
